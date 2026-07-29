895bebe9f81c12b40d73f03b1ac0f436bb06106c
Valentin Gonzalez <valentingonzalezlou@gmail.com>
2026-07-29 14:44:04 -0400

    feat: favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,27 +2,25 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Estora",
-    short_name: "Estora",
-    description: "Auto generative CRM",
+    name: "Brokerage",
+    short_name: "Brokerage",
+    description: "Best marketplace for roofing businesses",
     start_url: "/",
     display: "standalone",
-    background_color: "#214338",
+    background_color: "#ffffff",
     theme_color: "#ffffff",
     scope: "/",
-    orientation: "portrait", //
+    orientation: "portrait",
     icons: [
       {
-        src: "/web-app-manifest-192x192.png",
+        src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
       },
       {
-        src: "/web-app-manifest-512x512.png",
+        src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable",
       },
     ],
     prefer_related_applications: false,
